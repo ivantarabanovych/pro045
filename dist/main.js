a97f713712efaed2e920312a721ca4bcdb74ab3f
ivantarabanovych <vanjokcitet334@gmail.com>
2024-11-23 19:52:32 +0200

    q5

--- dist/main.js
+++ dist/main.js
@@ -1,24 +1,32 @@
 "use strict";
-var OrderStatus;
-(function (OrderStatus) {
-    OrderStatus["Pending"] = "Pendeng";
-    OrderStatus["Shipped"] = "Shipped";
-    OrderStatus["Delivered"] = "Delivered";
-    OrderStatus["Cencelled"] = "Cancelled";
-})(OrderStatus || (OrderStatus = {}));
-function getOrderStatusMessage(status) {
-    switch (status) {
-        case OrderStatus.Pending:
-            return "Ваше замовдення в обробці";
-        case OrderStatus.Shipped:
-            return "Ваше замовлення відправлено";
-        case OrderStatus.Delivered:
-            return "Ваше замовлення доставлено";
-        case OrderStatus.Cencelled:
-            return "Ваше замовлення доставлено";
-        default:
-            return "Невідомий статус замовлення";
-    }
-}
-var status = OrderStatus.Shipped;
-console.log(getOrderStatusMessage(status));
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
+var updateProduct = function (id, updates) {
+    console.log("\u041E\u043D\u043E\u0432\u043B\u0435\u043D\u043D\u044F \u043F\u0440\u043E\u0434\u0443\u043A\u0442\u0443 \u0437 ID: ".concat(id, ":"), updates);
+};
+var getProductSummary = function (product) {
+    var description = product.description, summary = __rest(product, ["description"]);
+    return summary;
+};
+var product = {
+    id: 1,
+    name: "Lenovo Yoga 32",
+    description: "Full HD screen, RAM: 16GB",
+    price: 1200
+};
+var updates = {
+    price: 1400,
+    name: "MacBookAir M3"
+};
+updateProduct(product.id, updates);
+var productSummary = getProductSummary(product);
+console.log("Короткий опис продукту:", productSummary);
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,38 @@
-enum OrderStatus {
-    Pending = "Pendeng",
-    Shipped = "Shipped",
-    Delivered = "Delivered",
-    Cencelled = "Cancelled"
-}
+type Product = {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+};
 
-function getOrderStatusMessage(status: OrderStatus): string{
-    switch(status){
-        case OrderStatus.Pending:
-            return "Ваше замовдення в обробці";
-        case OrderStatus.Shipped:
-            return "Ваше замовлення відправлено";
-        case OrderStatus.Delivered:
-            return "Ваше замовлення доставлено";
-        case OrderStatus.Cencelled:
-            return "Ваше замовлення доставлено";
-        default:
-            return "Невідомий статус замовлення";
-    }
-}
+type ProductUpdate = Partial<Product>;
 
-const oderStatus: OrderStatus = OrderStatus.Shipped; 
-console.log(getOrderStatusMessage(oderStatus));
+type ProductSummary = Omit<Product, 'description'>;
+
+const updateProduct = (id: number, updates: ProductUpdate): void => {
+    console.log(`Оновлення продукту з ID: ${id}:`, updates);
+};
+
+const getProductSummary = (product: Product): ProductSummary => {
+    const { description, ...summary } = product;
+    return summary;
+};
+
+const product: Product = {
+    id: 1,
+    name: "Lenovo Yoga 32",
+    description: "Full HD screen, RAM: 16GB",
+    price: 1200
+};
+
+const updates: ProductUpdate = {
+    price: 1400,
+    name: "MacBookAir M3"
+};
+
+updateProduct(product.id, updates);
+
+const productSummary: ProductSummary = getProductSummary(product);
+console.log("Короткий опис продукту:", productSummary);
+
+
